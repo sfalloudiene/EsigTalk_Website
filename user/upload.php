deb369ad86329d60f34bbd1644f700539a88ce27
Falu jene <74065196+sfalloudiene@users.noreply.github.com>
2024-12-16 22:24:21 +0100

    ajout d'un fichier gitignore

--- user/upload.php
+++
@@ -1,7 +0,0 @@
-$chemin_fichier = "uploads/" . $nom_fichier;
-if (!file_exists("uploads")) {
-    mkdir("uploads", 0777, true);
-}
-if (move_uploaded_file($_FILES['document']['tmp_name'], $chemin_fichier)) {
-    $piece_jointe = $chemin_fichier;
-}
